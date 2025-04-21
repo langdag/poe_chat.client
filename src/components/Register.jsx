726ed9688429bd99ab48e967b596c2fd6d012147
flamehive <stipanenkosasha@gmail.com>
2025-04-21 15:38:56 +0300

    Fix registration path

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -19,7 +19,7 @@ function Register() {
         }
 
         try {
-            const response = await fetch("http://localhost:8080/register", {
+            const response = await fetch("http://localhost:8080/registration", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ username, email, password }),
